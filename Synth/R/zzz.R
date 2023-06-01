e9b4aa320f849ccfea92cbb3f8a215f005bc0417
pako841212 <80023806+pako841212@users.noreply.github.com>
2023-06-01 11:10:57 -0700

    Add files via upload

---
+++ Synth/R/zzz.R
@@ -0,0 +1,9 @@
+.onAttach <-
+function (libname, pkgname)
+{
+   # echo output to screen
+   packageStartupMessage("##\n## Synth Package: Implements Synthetic Control Methods.\n")
+   packageStartupMessage("## See https://web.stanford.edu/~jhain/synthpage.html for additional information.\n\n")
+}
+
+
