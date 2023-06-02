d7c4cafd22172c901686caa928ec9b0614dde8bc
Nepo Rojas • GECI <braulio.rojas@islas.org.mx>
2023-06-01 19:45:48 -0700

    🔥 Delete repeated code

--- Synth/R/zzz.R
+++
@@ -1,9 +0,0 @@
-.onAttach <-
-function (libname, pkgname)
-{
-   # echo output to screen
-   packageStartupMessage("##\n## Synth Package: Implements Synthetic Control Methods.\n")
-   packageStartupMessage("## See https://web.stanford.edu/~jhain/synthpage.html for additional information.\n\n")
-}
-
-
